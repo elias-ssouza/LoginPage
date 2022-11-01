915c53fb0cd8a2da9de26ea8c2322e48e73e4aa5
elias-ssouza <elias.ssouza@hotmail.com>
2022-11-01 01:12:07 -0300

    tela de alterar senha

---
+++ app/src/main/java/com/example/loginpage/ChangePasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.example.loginpage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.loginactivity.R
+
+class ChangePasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_password)
+    }
+}
---
+++ app/src/main/java/com/example/loginpage/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.loginpage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.loginactivity.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
